9b6087d1bfb1222840d65a7ff7f3ba5823ccab91
Apple <1234@mac.mynetworksettings.com>
2026-04-26 08:37:47 -0700

    Fix application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#Stopping existing node servers
+# Stopping existing node servers
 echo "Stopping any existing node servers"
-pkill node || true
+pkill node 2>/dev/null || true
+echo "Application stop completed"
